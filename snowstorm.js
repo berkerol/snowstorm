06b9e2d2ed258c328ef2dd23f6edeaf5e3cf6ee1
Berk Erol <berk.erol@boun.edu.tr>
2018-09-30 00:05:50 +0300

    Add FPSMeter

--- snowstorm.js
+++ snowstorm.js
@@ -1,4 +1,5 @@
 /* global performance */
+/* global FPSMeter */
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -8,6 +9,14 @@ const getTime = typeof performance === 'function' ? performance.now : Date.now;
 const FRAME_DURATION = 1000 / 58;
 let then = getTime();
 let acc = 0;
+let meter = new FPSMeter({
+  left: canvas.width - 130 + 'px',
+  top: 'auto',
+  bottom: '12px',
+  theme: 'colorful',
+  heat: 1,
+  graph: 1
+});
 
 let flake = {
   color: '#FFFFFF',
@@ -48,6 +57,7 @@ function draw () {
   } else {
     ms = 0;
   }
+  meter.tick();
   ctx.lineCap = flake.lineCap;
   ctx.strokeStyle = flake.color;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
